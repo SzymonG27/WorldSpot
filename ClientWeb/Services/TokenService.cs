842945958057548e5d835f4e86184585d6601285
SzymonG27 <s.gajdzinski123@gmail.com>
2022-09-17 16:12:04 +0200

    Add token service

---
+++ ClientWeb/Services/TokenService.cs
@@ -0,0 +1,42 @@
+﻿using IdentityModel.Client;
+using Microsoft.Extensions.Options;
+
+namespace ClientWeb.Services
+{
+    public class TokenService : ITokenService
+    {
+        public readonly IOptions<IdentityServerSettings> identityServerSettings;
+        public readonly DiscoveryDocumentResponse discoveryDocument;
+        private readonly HttpClient client;
+
+        public TokenService(IOptions<IdentityServerSettings> identityServerSettings)
+        {
+            this.identityServerSettings = identityServerSettings;
+            client = new HttpClient();
+            this.discoveryDocument = client.GetDiscoveryDocumentAsync(this.identityServerSettings.Value.DiscoveryUrl).Result;
+
+            if (discoveryDocument.IsError)
+            {
+                throw new Exception("Nie można pobrać OpenID Discovery Document", discoveryDocument.Exception);
+            }
+        }
+
+        public async Task<TokenResponse> GetToken(string scope)
+        {
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = discoveryDocument.TokenEndpoint,
+                ClientId = identityServerSettings.Value.ClientName,
+                ClientSecret = identityServerSettings.Value.ClientPassword,
+                Scope = scope
+            });
+
+            if (tokenResponse.IsError)
+            {
+                throw new Exception("Nie można pobrać tokenu klienta", tokenResponse.Exception);
+            }
+
+            return tokenResponse;
+        }
+    }
+}
